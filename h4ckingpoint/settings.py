40b84c8a30ea4cc14ac5f630e592d04195f1b6c3
Miguel Angel Perez <107451329+theloganlab@users.noreply.github.com>
2023-08-26 11:50:44 -0500

    Update settings.py

--- h4ckingpoint/settings.py
+++ h4ckingpoint/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*slm*reaxpav+e&*2h&(y8r^*f(372#5n6witl6yf&5b4eqhgq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
